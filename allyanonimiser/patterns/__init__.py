ed491f045ecfae112c5ac76fda102c8cabe2c7cf
Stephen Oates <stephen.j.a.oates@gmail.com>
2025-03-03 11:53:35 +1100

    Improve patterns package organization

--- allyanonimiser/patterns/__init__.py
+++ allyanonimiser/patterns/__init__.py
@@ -1,3 +1,11 @@
 """
 Patterns package for the Allyanonimiser package.
+
+This package contains predefined patterns for entity detection.
 """
+
+from .au_patterns import get_au_pattern_definitions
+from .general_patterns import get_general_pattern_definitions
+from .insurance_patterns import get_insurance_pattern_definitions
+
+__all__ = ["get_au_pattern_definitions", "get_general_pattern_definitions", "get_insurance_pattern_definitions"]
